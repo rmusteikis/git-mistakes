cd65495be3eae055be4c8cf09eed26ffe1901aa5
Ramūnas Musteikis <ramunasm@wix.com>
2020-09-25 10:27:55 +0300

    refactor and update

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
 // app js code
-
 function helloWorld() {
   alert("hello world!");
 }
 
 function secondFunction() {
   alert("This is number 2");
-  alert("change 1");
-  alert("change 2");
-  alert("change 3");
 }
